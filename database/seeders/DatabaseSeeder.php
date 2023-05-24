64f3020bf56b93e7526aeba2b59eb405c3d0dbc1
Nand Vadera <127087495+nandvadera1@users.noreply.github.com>
2023-05-24 17:03:10 +0530

    Admin login (#4)
    * AdminLogin
    
    * AdminLogin2
    
    * Updated model name
    
    * dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\UserType;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        UserType::create([
+            'type' => 'Administrator'
+        ]);
+
+        UserType::create([
+            'type' => 'User'
+        ]);
+
+        User::create([
+            'user_type_id' => 1,
+            'name' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('secret')
+        ]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
