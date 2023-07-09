1829343f3b46474ec4ebd374e074331e6c193442
Nand Vadera <nandvadera25@gmail.com>
2023-07-09 13:23:13 +0530

    form

--- resources/views/vouchers/_form.blade.php
+++ resources/views/vouchers/_form.blade.php
@@ -2,7 +2,7 @@
     <div class="form-group row">
         {!! Form::label('campaign_id', 'Campaign', ['class' => 'col-sm-2 col-form-label']) !!}
         <div class="col-sm-10">
-            {!! Form::select('campaign_id', $campaignId, $campaignId ?? null, ['class' => 'form-control', 'required', 'placeholder' => 'Select a type']) !!}
+            {!! Form::select('campaign_id', $campaignId, $campaignId ?? null, ['class' => 'form-control', 'required', 'placeholder' => 'Select a campaign']) !!}
         </div>
         @error('password')
         <p class="text-danger text-xs mt-1">
@@ -14,7 +14,7 @@
     <div class="form-group row">
         {!! Form::label('number', 'Number of Vouchers', ['class' => 'col-sm-2 col-form-label']) !!}
         <div class="col-sm-10">
-            {!! Form::number('amount', null, ['class' => 'form-control', 'required', 'placeholder' => "Enter amount"]) !!}
+            {!! Form::number('amount', null, ['class' => 'form-control', 'required', 'placeholder' => "Enter number of vouchers"]) !!}
         </div>
         @error('amount')
         <p class="text-danger text-xs mt-1">
