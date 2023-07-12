eb8ac04a63605448842c40c903c0b79f3da19fe1
Nand Vadera <nandvadera25@gmail.com>
2023-07-12 13:33:16 +0530

    made changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(Excel::class, function ($app) {
-            return new VouchersExport($app['phpexcel']);
-        });
+        //
     }
 
     /**
