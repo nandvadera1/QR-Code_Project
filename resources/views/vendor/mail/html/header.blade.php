487995b49ba919d3826e19f5a3eb9b90daa3c918
Nand Vadera <nandvadera25@gmail.com>
2023-07-18 14:00:48 +0530

    verification

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a  style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="" class="logo" alt="Nand Logo">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
